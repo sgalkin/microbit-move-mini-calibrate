296cd45099061654fd429fc727eed496b7ec55aa
Sergey Galkin <sgalkin@users.noreply.github.com>
2022-11-05 23:46:01 +0100

    add calibaration blocks

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    basic.pause(1000)
+    kitronik_servo_lite.forward()
+    basic.pause(1000)
+    kitronik_servo_lite.stop()
+})
+input.onButtonPressed(Button.B, function () {
+    basic.pause(1000)
+    kitronik_servo_lite.left()
+    basic.pause(1000)
+    kitronik_servo_lite.stop()
 })
